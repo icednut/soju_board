d875ded7bbbed74a3b51961de33e544f7401f410
Wangeun Lee <wglee21g@gmail.com>
2012-12-28 14:44:40 +0900

    소스 Commit 테스트

--- board/src/test/java/com/nhncorp/study/board/controller/ArticleControllerTest.java
+++ board/src/test/java/com/nhncorp/study/board/controller/ArticleControllerTest.java
@@ -1,24 +1,29 @@
 package com.nhncorp.study.board.controller;
 
-import org.junit.Test;
-import static org.springframework.test.web.server.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.server.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.server.result.MockMvcResultMatchers.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Ignore;
+import org.junit.Test;
 import org.springframework.test.web.server.setup.MockMvcBuilders;
 
-import com.nhncorp.study.board.controller.ArticleController;
 import com.nhncorp.study.board.model.Article;
 import com.nhncorp.study.board.model.Member;
 
 public class ArticleControllerTest {
 
 	@Test
+	@Ignore
 	public void testGetArticleForm() throws Exception {
 		Article article = new Article();
 		Member member = new Member();
 		member.setId("crazybnn");
 		member.setName("Lee");
 		article.setMember(member);
-		
+
 		MockMvcBuilders.standaloneSetup(new ArticleController()).build()
 				.perform(get("/form")).andExpect(status().isOk())
 				.andExpect(view().name("article/form"))
@@ -26,36 +31,61 @@ public class ArticleControllerTest {
 	}
 
 	@Test
+	@Ignore
 	public void testSaveArticlXteForm() throws Exception {
 		MockMvcBuilders
 				.xmlConfigSetup(("classpath:servlet-context.xml"))
 				.build()
 				.perform(
-						post("/form")
-							.param("title", "hello title")
-							.param("contents", "hello contents")
-							.param("member.id", "crazybnn"))
+						post("/form").param("title", "hello title")
+								.param("contents", "hello contents")
+								.param("member.id", "crazybnn"))
 				.andExpect(view().name("article/view"));
 	}
 
-//	@Test
-//	public void testSaveArticlXteForm_¡¶∏Ò¿Ã_æ¯¿ª∞ÊøÏ() throws Exception {
-//		Article expectArticle = new Article();
-//		expectArticle.setTitle("hello title");
-//		expectArticle.setContents("hello contents");
-//		
-//		MockMvcBuilders
-//				.xmlConfigSetup(("classpath:servlet-context.xml"))
-//				.build()
-//				.perform(
-//						post("/form")
-//						.param("title", "hello title")
-//						.param("contents", "hello contents")
-////						.param("member.id", "crazybnn")
-//				)
-//				.andExpect(status().isOk())
-//				.andExpect(view().name("article/form"))
-////				.andExpect(model().attributeHasFieldErrors("article", "member"))
-//				.andExpect(model().attribute("article", expectArticle));
-//	}
+	// @Test
+	// public void testSaveArticlXteForm_ÔøΩÔøΩÔøΩÔøΩÔøΩÔøΩ_ÔøΩÔøΩÔøΩÔøΩÔøΩÔøΩÔøΩ() throws Exception {
+	// Article expectArticle = new Article();
+	// expectArticle.setTitle("hello title");
+	// expectArticle.setContents("hello contents");
+	//
+	// MockMvcBuilders
+	// .xmlConfigSetup(("classpath:servlet-context.xml"))
+	// .build()
+	// .perform(
+	// post("/form")
+	// .param("title", "hello title")
+	// .param("contents", "hello contents")
+	// // .param("member.id", "crazybnn")
+	// )
+	// .andExpect(status().isOk())
+	// .andExpect(view().name("article/form"))
+	// // .andExpect(model().attributeHasFieldErrors("article", "member"))
+	// .andExpect(model().attribute("article", expectArticle));
+	// }
+
+	@Test
+	public void testGetArticles_Í≤åÏãúÍ∏Ä_Ï≤´Î™©Î°ù_ÏñªÏñ¥Ïò§Í∏∞_ÏÑ±Í≥µ() throws Exception {
+		// given
+		List<Article> expectArticleList = new ArrayList<Article>();
+		Member member = new Member();
+		member.setId("crazybnn");
+		member.setName("ÔøΩÃøœ±ÔøΩ");
+		Article article = new Article();
+		article.setTitle("ÔøΩÔøΩÔøΩÔøΩ1");
+		article.setContents("ÔøΩÔøΩÔøΩÔøΩ1 ÔøΩÔøΩÔøΩÔøΩ");
+		article.setMember(member);
+		article.setRegYmdt("2012-12-20 11:23:01");
+		expectArticleList.add(article);
+
+		// when & then
+		MockMvcBuilders
+				.xmlConfigSetup("classpath:root-context.xml",
+						"classpath:servlet-context.xml",
+						"classpath:datasource-context.xml",
+						"classpath:mybatis-context.xml").build()
+				.perform(post("/article/list").param("page", "1"))
+				.andExpect(status().isOk())
+				.andExpect(model().attribute("articles", expectArticleList));
+	}
 }
